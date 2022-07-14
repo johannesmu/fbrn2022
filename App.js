58bd319de58b6b590cdbbeef12d434c7430c45e7
johannes <jmuljana@gmail.com>
2022-07-14 13:56:31 +1000

    added firebase and metro.config file

--- App.js
+++ App.js
@@ -1,6 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
+// firebase config
+import { firebaseConfig } from './config/Config'
+import { initializeApp } from 'firebase/app'
+// initialise firebase app
+initializeApp( firebaseConfig )
+
 export default function App() {
   return (
     <View style={styles.container}>
---
+++ config/Config.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyCjAHYWYHv1A_t8frPG8aFVSXINH-Mzy0M",
+  authDomain: "fbrn22-6205b.firebaseapp.com",
+  projectId: "fbrn22-6205b",
+  storageBucket: "fbrn22-6205b.appspot.com",
+  messagingSenderId: "230182997566",
+  appId: "1:230182997566:web:63dd963fcc5de1d54e8b6f",
+  measurementId: "G-STLYWCBH4J"
+}
