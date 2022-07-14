18ca79fa2dc99e83df39510bfd76ad45a0ca200c
johannes <jmuljana@gmail.com>
2022-07-14 15:11:47 +1000

    added navigation to signin

--- screens/SignupScreen.js
+++ screens/SignupScreen.js
@@ -1,8 +1,12 @@
-import { Text, View, StyleSheet} from 'react-native'
-export function SignupScreen( props ) {
+import { Text, View, StyleSheet, TouchableOpacity } from 'react-native'
+export function SignupScreen( {navigation} ) {
+
   return (
     <View style={styles.signupView}>
       <Text>Sign up Screen</Text>
+      <TouchableOpacity onPress={ () => navigation.navigate('Signin') }>
+        <Text>Go to Sign in</Text>
+      </TouchableOpacity>
     </View>
   )
 }
