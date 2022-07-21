e3503d5b35852addd53eb5e6321fb3d2097e2de1
johannes <jmuljana@gmail.com>
2022-07-21 13:54:59 +1000

    adding a signup screen

--- screens/SignupScreen.js
+++ screens/SignupScreen.js
@@ -1,13 +1,22 @@
-import { Text, View, StyleSheet, TouchableOpacity } from 'react-native'
+import { Text, TextInput, View, StyleSheet, TouchableOpacity, KeyboardAvoidingView } from 'react-native'
 export function SignupScreen( {navigation} ) {
 
   return (
-    <View style={styles.signupView}>
-      <Text>Sign up Screen</Text>
+    <KeyboardAvoidingView style={styles.signupView} behavior='padding'>
+      <Text>Sign up</Text>
+      <View style={styles.signupForm}>
+      <Text style={styles.label}>Email</Text>
+      <TextInput style={styles.input} />
+      <Text>Password</Text>
+      <TextInput style={styles.input} />
+      <TouchableOpacity>
+        <Text>Sign up</Text>
+      </TouchableOpacity>
+      </View>
       <TouchableOpacity onPress={ () => navigation.navigate('Signin') }>
         <Text>Go to Sign in</Text>
       </TouchableOpacity>
-    </View>
+    </KeyboardAvoidingView>
   )
 }
 
@@ -17,4 +26,20 @@ const styles = StyleSheet.create( {
     justifyContent: 'center',
     alignItems: 'center',
   },
+  signupForm: {
+    backgroundColor: "lightblue",
+    width: '80%',
+    padding: 10,
+  },
+  label: {
+    marginVertical: 10,
+  },
+  input: {
+    backgroundColor: '#ffffff',
+    marginBottom: 15,
+    padding: 10,
+  },
+  form: {
+    alignItems: 'flex-start',
+  }
 } )
