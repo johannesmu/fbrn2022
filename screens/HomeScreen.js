44139f2e9a2f9eeb5d7d8fc21b560ef88d57ac5e
johannes <jmuljana@gmail.com>
2022-08-11 13:49:27 +1000

    adding data to Firestore

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,10 +1,17 @@
 import { Text, View, StyleSheet, TextInput, TouchableOpacity} from 'react-native'
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 import { useNavigation } from '@react-navigation/native'
 
 
 export function HomeScreen( props ) {
   const navigation = useNavigation()
+
+  const [input,setInput] = useState()
+
+  const submit = ( path, data ) => {
+    const dataObj = {name: data, date: new Date() }
+    props.add( path, dataObj )
+  }
 
   useEffect( () => {
     if( !props.auth ) {
@@ -14,8 +21,11 @@ export function HomeScreen( props ) {
 
   return (
     <View style={styles.homeView}>
-      <TextInput style={styles.input} />
-      <TouchableOpacity style={styles.button}>
+      <TextInput style={styles.input} onChangeText={ (val) => setInput(val) } />
+      <TouchableOpacity 
+        style={styles.button}
+        onPress={ () => { submit(`users/${props.auth.uid}/items`,input) }}
+      >
         <Text>Add</Text>
       </TouchableOpacity>
     </View>
