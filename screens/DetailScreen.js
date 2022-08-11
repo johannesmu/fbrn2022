1f969330825d9d88e21e468fa76a5f5fa11a234f
johannes <jmuljana@gmail.com>
2022-08-11 15:03:56 +1000

    passing data to detail screen

---
+++ screens/DetailScreen.js
@@ -0,0 +1,13 @@
+import {View, Text, StyleSheet} from 'react-native'
+import { useRoute } from '@react-navigation/native'
+export function DetailScreen( props ) {
+  const route = useRoute()
+  const { id, name } = route.params
+
+  return (
+    <View>
+      <Text>{ id }</Text>
+      <Text>{ name }</Text>
+    </View>
+  )
+}
